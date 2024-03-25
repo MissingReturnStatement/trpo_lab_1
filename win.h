c345cfeb230879b223890149c1c3d40214396615
deaddude <denchixonfirst@gmail.com>
2024-03-25 22:10:36 +0700

    test commit

---
+++ win.h
@@ -0,0 +1,39 @@
+#ifndef WIN_H
+#define WIN_H
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
+class Win:public QWidget // класс окна
+//наследуемся от Qwidget
+{
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
+public:
+    Win(QWidget *parent = 0); // конструктор по умолчанию
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
+};
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда принимает вводимую строку
+    }
+};
+#endif // WIN_H
